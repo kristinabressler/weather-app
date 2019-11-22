d752d92e32d17fd1c5e8b0f68f442c486b911cb3
kikibres <kristinabressler@yahoo.com>
2019-11-22 10:13:46 -0500

    updating

---
+++ src/components/restaurant-detail.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export class RestaurantDetail extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default RestaurantDetail
